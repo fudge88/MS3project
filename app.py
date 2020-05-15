4199a596456c70ec937048957cba2d08295652f8
fudge88 <f.akhlaq@outlook.com>
2020-05-15 17:07:03 +0000

    registrtaion page created

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = '334917b981349a5d0256a084221e6d5dd164'
 
 @app.route("/")
 @app.route("/home")
@@ -13,6 +14,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = RegistrationForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__  == '__main__':
