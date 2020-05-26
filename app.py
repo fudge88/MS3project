cd84848d6830f02065519b9f0eb4d696a1de0d28
fudge88 <f.akhlaq@outlook.com>
2020-05-26 18:26:39 +0000

    user being pushed to mongo db as new user

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, render_template, url_for, flash, redirect, session
+from flask import Flask, render_template, url_for, flash, redirect, session, request
 from forms import RegistrationForm, LoginForm
 if os.path.exists('env.py'):
     import env
@@ -23,6 +23,11 @@ def index():
     return render_template('login.html', title='Login', form=form)
 
 
+@app.route('/get_drinks')
+def get_drinks():
+    return render_template('display.html', drinks=mongo.db.drinks.find())
+
+
 @app.route("/home")
 def home():
     return render_template('index.html')
@@ -32,19 +37,39 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+#User Register 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if 'username' in session:
+        flash('You are already logged in!')
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Welcome to the family {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        account = mongo.db.users
+        existing_account = mongo.db.users.find_one({
+                                                'username': request.form['username']})
+        if existing_account:
+            flash(f'Welcome back {form.username.data}!', 'success')
+            return redirect(url_for('home'))
+        else:
+            new_account = { 'username': request.form['username']}
+            account.insert_one(new_account)
+            session['username'] = request.form['username']
+            flash(f'Welcome to the family {form.username.data}!', 'success')
+            return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == "fujeh":
+            flash('you have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsucessful. Please register.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
