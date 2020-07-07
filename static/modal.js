a19772d8e2e47f79669d28334bc299163fcd4699
fudge88 <f.akhlaq@outlook.com>
2020-07-07 12:51:30 +0000

    modal now working as it has been looped into the original for loop

--- static/modal.js
+++
@@ -1,7 +0,0 @@
-// class Modal {
-//     constructor (){
-//         this.modalContainer = document.createElement('div');
-//         this.modalContainer.className = 'modal';
-//         document.body.appendChild(this)
-//     }
-// }
